78c27e88140025645114e2afc3c9e9daa94a765d
Himanshu Jha <115359400+CoderHimanshu2002@users.noreply.github.com>
2024-03-15 20:49:45 +0530

    Update LCAInBST.cpp
    You are given a binary search tree of integers with N nodes. You are also given references to two nodes 'P' and 'Q' from this BST.
    
    
    
    Your task is to find the lowest common ancestor(LCA) of these two given nodes.
    
    
    
    The lowest common ancestor for two nodes P and Q is defined as the lowest node that has both P and Q as descendants (where we allow a node to be a descendant of itself)
    
    
    
    A binary search tree (BST) is a binary tree data structure which has the following properties.
    
    • The left subtree of a node contains only nodes with data less than the node’s data.
    • The right subtree of a node contains only nodes with data greater than the node’s data.
    • Both the left and right subtrees must also be binary search trees.

--- LCAInBST.cpp
+++ LCAInBST.cpp
@@ -1,3 +1,4 @@
+//APPROACH 1-: (Recursive Way)...
 TreeNode *LCAinaBST(TreeNode *root, TreeNode *P, TreeNode *Q)
 {
     // Write your code here.
@@ -15,3 +16,16 @@ TreeNode *LCAinaBST(TreeNode *root, TreeNode *P, TreeNode *Q)
 
     return root; // Else the root will be the LCA of P and Q...
 }
+
+// APPROACH 2 -: (Iterative Way)...
+while(root){
+
+        if(root->data > P->data && root->data > Q->data)
+        root=root->left;
+
+        if(root->data < P->data && root->data < Q->data)
+        root=root->right;
+
+        else
+        return root;
+    }
